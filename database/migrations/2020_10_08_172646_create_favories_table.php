dfad367ec09ec6a155f670a6ec1a5fafd6a0b855
dev James <adeolajames93@gmail.com>
2020-10-27 20:57:55 +0100

    changing migrations

--- database/migrations/2020_10_08_172646_create_favories_table.php
+++ database/migrations/2020_10_08_172646_create_favories_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 class CreateFavoriesTable extends Migration
@@ -30,6 +31,9 @@ class CreateFavoriesTable extends Migration
      */
     public function down()
     {
+        DB::statement('SET FOREIGN_KEY_CHECKS = 0');
         Schema::dropIfExists('favories');
+        DB::statement('SET FOREIGN_KEY_CHECKS = 1');
+
     }
 }
